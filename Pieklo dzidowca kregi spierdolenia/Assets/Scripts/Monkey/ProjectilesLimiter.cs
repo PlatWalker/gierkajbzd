68da10e048bf2a142efca5eb79a19817cc8b500c
kumdzio <kumdzio@gmail.com>
2020-11-03 22:26:48 +0100

    Projectiles limiter
    Dodany limit pociksów małpy
    Zostało do poprawienia tylko sposób zmiany ilość limitowanych pocisków

---
+++ Pieklo dzidowca kregi spierdolenia/Assets/Scripts/Monkey/ProjectilesLimiter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectilesLimiter : MonoBehaviour
+{
+    private static long nextID = 0;
+    private static long IdToBeDestroyed = -1;
+    private long ID;
+    [SerializeField] private int projectilesLimit=10;
+    private int pLimit;
+
+    public ProjectilesLimiter()
+    {
+        ID = nextID;
+        nextID++;
+        pLimit = projectilesLimit;
+        if (ID >= projectilesLimit)
+        {
+            IdToBeDestroyed = ID - pLimit;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.ID <= IdToBeDestroyed)
+        {
+            Destroy(this.gameObject);
+        }   
+    }
+}
