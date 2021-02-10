8d347e0d61c49f7ca83112c1446783bf1666aa87
Sharashino <sharashino@icloud.com>
2021-02-10 18:10:43 +0100

    Import Text Mesh Pro Create Working NPC with dialogue Create Dialogue menu for player

---
+++ Pieklo dzidowca kregi spierdolenia/Assets/Scripts/Managers/DialogueManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using jbzdy.DialogueSystem.DataContainers;
+using System.Linq;
+using System;
+
+namespace jbzdy.DialogueSystem.managers
+{
+    public class DialogueManager : MonoBehaviour
+    {
+        [SerializeField] private DialogueContainer dialogueToNarrate;
+        [SerializeField] private TextMeshProUGUI dialogueText;
+        [SerializeField] private Button choiceButtonPrefab;
+        [SerializeField] private Transform buttonContainer;
+
+        private void Start()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void StartDialogue(DialogueContainer dialogue)
+        {
+            gameObject.SetActive(true);
+
+            dialogueToNarrate = dialogue;
+            NodeLinkData dialogueData = dialogueToNarrate.NodeLinks.First();
+
+            NarrateDialogue(dialogueData.TargetNodeGuid);
+        }
+
+        private void NarrateDialogue(string nodeGUID)
+        {
+            var text = dialogueToNarrate.DialogueNodeDatas.Find(x => x.NodeGUID == nodeGUID).DialogueText;
+            var answerChoices = dialogueToNarrate.NodeLinks.Where(x => x.BaseNodeGuid == nodeGUID);
+            dialogueText.text = ProcessProperties(text);
+            var buttons = buttonContainer.GetComponentsInChildren<Button>();
+
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                Destroy(buttons[i].gameObject);
+            }
+
+            foreach (var choice in answerChoices)
+            {
+                Button choiceButton = Instantiate(choiceButtonPrefab, buttonContainer);
+                choiceButton.GetComponentInChildren<TMP_Text>().text = ProcessProperties(choice.PortName);
+                choiceButton.onClick.AddListener(() => NarrateDialogue(choice.TargetNodeGuid));
+            }
+        }
+
+        private string ProcessProperties(string text)
+        {
+            foreach (var exposedProperty in dialogueToNarrate.ExposedProperties)
+            {
+                text = text.Replace($"[{exposedProperty.PropertyName}]", exposedProperty.PropertyValue);
+            }
+            return text;
+        }
+    }
+
+}
---
+++ Pieklo dzidowca kregi spierdolenia/Assets/Scripts/NPC/NPCInteraction.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using jbzdy.DialogueSystem.managers;
+using jbzdy.DialogueSystem.DataContainers;
+
+namespace jbzdy.NPC.interaction
+{
+    public class NPCInteraction : Interactables
+    {
+        [SerializeField] private DialogueContainer NPCDialogue;
+        [SerializeField] private DialogueManager dialogueManager;
+
+        public override void Interact()
+        {
+            base.Interact();
+            NPCInterract();
+        }
+
+        private void NPCInterract()
+        {
+            dialogueManager.StartDialogue(NPCDialogue);
+        }
+    }
+}
