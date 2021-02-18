b3e9bd27077126f0f1cb25e77f3e94aa589da3e9
Sharashino <sharashino@icloud.com>
2021-02-18 23:48:22 +0100

    Move remaded DialogueSystem Delete old one

---
+++ Pieklo dzidowca kregi spierdolenia/Assets/Scripts/Dialogue Use/DialogueGetData.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class DialogueGetData : MonoBehaviour
+{
+    [SerializeField] protected DialogueContainerSO dialogueContainer;
+
+    protected BaseNodeData GetNodeByGuid(string _targetNodeGuid)
+    {
+        return dialogueContainer.AllNodes.Find(node => node.NodeGuid == _targetNodeGuid);
+    }
+
+    protected BaseNodeData GetNodeByNodePort(DialogueNodePort _nodePort)
+    {
+        return dialogueContainer.AllNodes.Find(node => node.NodeGuid == _nodePort.InputGuid);
+    }
+
+    protected BaseNodeData GetNextNode(BaseNodeData _baseNodeData)
+    {
+        NodeLinkData nodeLinkData = dialogueContainer.NodeLinkDatas.Find(egde => egde.BaseNodeGuid == _baseNodeData.NodeGuid);
+
+        return GetNodeByGuid(nodeLinkData.TargetNodeGuid);
+    }
+}
--- Pieklo dzidowca kregi spierdolenia/Assets/Scripts/NPC/NPCInteraction.cs
+++ Pieklo dzidowca kregi spierdolenia/Assets/Scripts/NPC/NPCInteraction.cs
@@ -1,23 +1,22 @@
 ﻿using UnityEngine;
-using jbzdy.DialogueSystem.managers;
-using jbzdy.DialogueSystem.DataContainers;
 
 namespace jbzdy.NPC.interaction
 {
     public class NPCInteraction : Interactables
     {
-        [SerializeField] private DialogueContainer NPCDialogue;
-        [SerializeField] private DialogueManager dialogueManager;
 
         public override void Interact()
         {
-            base.Interact();
-            NPCInterract();
+            if(true)
+            {
+                base.Interact();
+                NPCInterract();
+            }
         }
 
         private void NPCInterract()
         {
-            dialogueManager.StartDialogue(NPCDialogue);
+            
         }
     }
 }
