28af8ba46964e0d861a4af850360f119136075c6
Silver Walker <66061199+PlatWalker@users.noreply.github.com>
2021-04-26 18:53:38 +0200

    Animation Fix
    animation bug fix

--- Pieklo dzidowca kregi spierdolenia/Assets/Scripts/MainHero/Movement variation 2/PlayerController.cs
+++ Pieklo dzidowca kregi spierdolenia/Assets/Scripts/MainHero/Movement variation 2/PlayerController.cs
@@ -33,6 +33,7 @@ public class PlayerController : MonoBehaviour
     Vector3 attackVector = Vector3.zero;            // 
     Vector3 reflectedAttackVector = Vector3.zero;   // could be local. to change after merge 
     Vector3 animationVector = Vector3.zero;         // 
+    Vector3 moveVectorSaved = Vector3.zero;
 
     private void Awake()
     {
@@ -69,6 +70,43 @@ public class PlayerController : MonoBehaviour
 
     private void UpdateCharacterAttack()
     {
+
+        if (moveVectorSaved != movementVector && characterAnimator.GetBool("Attack") == true)
+        {
+            attackVector = InputController.Instance.mousePositionFlat - transform.position;
+
+            // reflect attack vector to compensate movement(momentum) vector
+
+            reflectedAttackVector = (2 * Vector3.Dot(movementVector.normalized, attackVector.normalized) * movementVector.normalized) - attackVector.normalized;
+
+            // rotate reflected vector to let it point a direction of animation to use. It's important for blender tree.
+
+            float Beta = -1;
+
+            if (movementVector == Vector3.forward) Beta = 0;
+            else if (movementVector == (Vector3.left + Vector3.forward)) Beta = 315;
+            else if (movementVector == Vector3.left) Beta = 270;
+            else if (movementVector == (Vector3.left + Vector3.back)) Beta = 225;
+            else if (movementVector == Vector3.back) Beta = 180;
+            else if (movementVector == (Vector3.back + Vector3.right)) Beta = 135;
+            else if (movementVector == Vector3.right) Beta = 90;
+            else if (movementVector == (Vector3.forward + Vector3.right)) Beta = 45;
+
+            if (Beta >= 0)
+            {
+                animationVector = Quaternion.AngleAxis(Beta, Vector3.down) * reflectedAttackVector;
+            }
+
+            moveVectorSaved = movementVector;
+
+            // start attack animation
+
+            transform.LookAt(InputController.Instance.mousePositionFlat);
+            characterAnimator.SetFloat("x", animationVector.x);
+            characterAnimator.SetFloat("z", animationVector.z);
+            characterAnimator.SetBool("Attack", true);
+
+        }
 
         if (InputController.Instance.attackInputStatus.normal == true)
         {
