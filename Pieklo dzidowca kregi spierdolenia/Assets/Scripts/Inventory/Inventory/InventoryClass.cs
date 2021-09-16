f2341a3dc593cf7ae1eb0a8ad262dee42cc25ca2
kumdzio <kumdzio@gmail.com>
2021-09-16 19:45:09 +0200

    final clean-up of not my code

--- Pieklo dzidowca kregi spierdolenia/Assets/Scripts/Inventory/Inventory/InventoryClass.cs
+++ Pieklo dzidowca kregi spierdolenia/Assets/Scripts/Inventory/Inventory/InventoryClass.cs
@@ -35,7 +35,7 @@ namespace jbzdy.Inventory
 
         #region utility
         public List<GridSlot> slots;
-        public Image cell;
+        [HideInInspector] public Image cell;
         [HideInInspector] public int cellSize = 70;
         [HideInInspector] public int padding;
         [HideInInspector] public int column = 5;
@@ -94,7 +94,6 @@ namespace jbzdy.Inventory
             {
                 for (int k = 0; k < equipmentPanels.Count; k++)
                 {
-                    Debug.Log("Wypełniam: " + k + " czyli: " + equipmentPanels[k].transform.name);
                     for (int i = 0; i < equipmentPanels[k].width; i++)
                     {
                         for (int j = 0; j < equipmentPanels[k].height; j++)
