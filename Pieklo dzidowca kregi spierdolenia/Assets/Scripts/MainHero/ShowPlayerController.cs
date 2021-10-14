334b25d5ba3050b64f05cbbff27008427f14719a
kumdzio <kumdzio@gmail.com>
2021-10-14 22:00:07 +0200

    ShaderDone
    Now only needs to put in all respecting models

---
+++ Pieklo dzidowca kregi spierdolenia/Assets/Scripts/MainHero/ShowPlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowPlayerController : MonoBehaviour
+{
+    public LayerMask Mask;
+    private Material hitedMaterial;
+    private int SizeID;
+
+    private void Start()
+    {
+        SizeID = Shader.PropertyToID("_size");
+    }
+
+    void Update()
+    {
+        Vector3 dir = GameManager.Instance.PlayerObject.transform.position - transform.position;
+        Ray ray = new Ray(transform.position, dir.normalized);
+        RaycastHit hitinfo;
+        if (Physics.Raycast(ray,out hitinfo, 100, Mask))
+        {
+            Debug.Log("uderzenie: "+hitinfo.transform.name);
+            Renderer renderer = hitinfo.transform.GetComponent<Renderer>();
+            if (!renderer)
+            {
+                if (hitedMaterial)
+                {
+                    hitedMaterial.SetFloat(SizeID, 0);
+                }
+                return;
+            }
+            hitedMaterial = renderer.material;
+            hitedMaterial.SetFloat(SizeID, 2);
+            if (hitedMaterial.shader.name!="Shader Graphs/show_player")
+            {
+                hitedMaterial.SetFloat(SizeID, 0);
+            }
+        }
+        else
+        {
+            if (hitedMaterial)
+            {
+                hitedMaterial.SetFloat(SizeID, 0);
+            }
+        }
+    }
+}
