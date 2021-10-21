33f494dda2ad2919a871e5942c6393dd899005cc
kumdzio <kumdzio@gmail.com>
2021-10-21 19:24:34 +0200

    Fluent Appear Effect
    Also some Z-index fix

--- Pieklo dzidowca kregi spierdolenia/Assets/Scripts/MainHero/ShowPlayerController.cs
+++ Pieklo dzidowca kregi spierdolenia/Assets/Scripts/MainHero/ShowPlayerController.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class ShowPlayerController : MonoBehaviour
 {
     public LayerMask Mask;
-    private Material hitedMaterial;
+    private Material hitedMaterial,lastHitedMaterial;
     private int SizeID;
+    [SerializeField] private float Size =2f;
+    [SerializeField] private float transitionDuration=2f;
 
+    private enum State {
+        notShowing,
+        startingToShow,
+        stoppingShowing,
+        showing
+    }
+
+    private State state;
     private void Start()
     {
         SizeID = Shader.PropertyToID("_size");
+        state = State.notShowing;
     }
 
     void Update()
@@ -20,29 +31,63 @@ public class ShowPlayerController : MonoBehaviour
         RaycastHit hitinfo;
         if (Physics.Raycast(ray,out hitinfo, 100, Mask))
         {
-            Debug.Log("uderzenie: "+hitinfo.transform.name);
             Renderer renderer = hitinfo.transform.GetComponent<Renderer>();
             if (!renderer)
             {
-                if (hitedMaterial)
-                {
-                    hitedMaterial.SetFloat(SizeID, 0);
-                }
-                return;
+                if (state == State.showing || state == State.startingToShow)
+                    state = State.stoppingShowing;
             }
-            hitedMaterial = renderer.material;
-            hitedMaterial.SetFloat(SizeID, 2);
-            if (hitedMaterial.shader.name!="Shader Graphs/show_player")
+            else
             {
-                hitedMaterial.SetFloat(SizeID, 0);
+                if (renderer.material.shader.name == "Shader Graphs/show_player")
+                {
+                    hitedMaterial = renderer.material;
+
+                    if (state == State.notShowing || state == State.stoppingShowing)
+                    {
+                        state = State.startingToShow;
+                    }
+                }
+                else
+                {
+                    if (state == State.showing || state == State.startingToShow)
+                        state = State.stoppingShowing;
+                }
             }
         }
         else
         {
-            if (hitedMaterial)
+            if (state == State.showing || state == State.startingToShow)
+                state = State.stoppingShowing;
+        }
+
+        float value;
+        switch (state)
+        {
+            case State.startingToShow:
+                if (!hitedMaterial) break;
+                value = hitedMaterial.GetFloat(SizeID);
+                value = value + Time.deltaTime / transitionDuration * Size;
+                hitedMaterial.SetFloat(SizeID, value);
+                if (value >= Size) state = State.showing;
+                break;
+            case State.stoppingShowing:
+                if (!hitedMaterial) break;
+                value = hitedMaterial.GetFloat(SizeID);
+                value = value - Time.deltaTime / transitionDuration * Size;
+                if (value < 0) value = 0;
+                hitedMaterial.SetFloat(SizeID, value);
+                if (value == 0) state = State.notShowing;
+                break;
+        }
+
+        if (hitedMaterial && lastHitedMaterial)
+        {
+            if (hitedMaterial != lastHitedMaterial)
             {
-                hitedMaterial.SetFloat(SizeID, 0);
+                lastHitedMaterial.SetFloat(SizeID, 0);
             }
         }
+        lastHitedMaterial = hitedMaterial;
     }
 }
