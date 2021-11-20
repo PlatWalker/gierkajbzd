81630baa9bd692ac956462808e2b75aaf727dd76
Silver Walker <66061199+PlatWalker@users.noreply.github.com>
2021-11-20 15:52:49 +0100

    fixed bug

--- Pieklo dzidowca kregi spierdolenia/Assets/Scripts/MainHero/PlayerMovementController.cs
+++ Pieklo dzidowca kregi spierdolenia/Assets/Scripts/MainHero/PlayerMovementController.cs
@@ -9,11 +9,13 @@ namespace jbzdy.Player
 {
     public class PlayerMovementController
     {
-        public PlayerController playerController;
+        private readonly PlayerController playerController;
+        private Vector3 newPositionVector;
 
         public PlayerMovementController(PlayerController _playerController)
         {
             playerController = _playerController;
+            
         }
 
         public void UpdateCharacterMovement()
@@ -34,12 +36,27 @@ namespace jbzdy.Player
             playerController.MovementVector += Vector3.left * Convert.ToInt32(GameManager.Instance.GameInputController.movementInputStatus.left);
             playerController.MovementVector += Vector3.right * Convert.ToInt32(GameManager.Instance.GameInputController.movementInputStatus.right);
 
-            playerController.transform.position += playerController.MovementVector.normalized * playerController.PlayerSpeed;
+            StickPlayerToGround();
+
+            playerController.rb.AddForce(playerController.MovementVector.normalized * playerController.PlayerSpeed * Time.fixedDeltaTime , ForceMode.VelocityChange);
+
+        }
+
+        private void StickPlayerToGround()
+        {
+            if (Physics.Raycast(playerController.transform.position + Vector3.up, Vector3.down, out RaycastHit hit))
+            {
+                newPositionVector.x = playerController.rb.position.x;
+                newPositionVector.y = hit.point.y;
+                newPositionVector.z = playerController.rb.position.z;
+
+                playerController.rb.MovePosition(newPositionVector);
+            }
         }
 
         private void UpdateCharacterRotation()
         {
-            if (playerController.MovementVector.magnitude == 0 || playerController.characterAnimator.GetBool("Attack") == true) return;
+            if (playerController.MovementVector.magnitude == 0 || playerController.CharacterAnimator.GetBool("Attack") == true) return;
 
             var rotation = Quaternion.LookRotation(playerController.MovementVector);
             playerController.transform.rotation = rotation;
@@ -49,11 +66,11 @@ namespace jbzdy.Player
         {
             if (playerController.MovementVector.z != 0 || playerController.MovementVector.x != 0)
             {
-                playerController.characterAnimator.SetBool("Run", true);
+                playerController.CharacterAnimator.SetBool("Run", true);
             }
             else
             {
-                playerController.characterAnimator.SetBool("Run", false);
+                playerController.CharacterAnimator.SetBool("Run", false);
             }
         }
     } 
