24481d085c5e73ff617dc08925b717e1127008e4
Mawerty <kontodzidastudio+4@gmail.com>
2024-01-14 16:40:29 +0100

    Pull request #283: GD 875 gra nie dziala po odpaleniu menu
    Merge in GD/gierka-jbzd from bugfix/GD-875-gra-nie-dziala-po-odpaleniu-menu to tech-demo
    
    Squashed commit of the following:
    
    commit 9cdb90fb725e5fad460615d06f412c95065b8a36
    Author: mawerty <krzysiek.pniaczek@interia.pl>
    Date:   Sun Jan 14 16:37:31 2024 +0100
    
        Revert "Naprawienie"
    
        This reverts commit f5fe99601c8a6934f7856ff248af91ff7dc8eae4.
    
    commit f5fe99601c8a6934f7856ff248af91ff7dc8eae4
    Author: mawerty <krzysiek.pniaczek@interia.pl>
    Date:   Sun Jan 14 15:44:20 2024 +0100
    
        Naprawienie

--- Pieklo dzidowca kregi spierdolenia/Assets/Scripts/UI/MainMenu/MainMenu.cs
+++ Pieklo dzidowca kregi spierdolenia/Assets/Scripts/UI/MainMenu/MainMenu.cs
@@ -1,35 +1,39 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using jbzd.MainHero;
 namespace jbzd.UI.MainMenu
 {
     public class MainMenu : MonoBehaviour
     {
         public GameObject eventSystem;
         public GameObject mainCamera;
+        private PlayerManager _playerManager;
 
         public void PlayGame()
         {
             eventSystem.SetActive(false);
             mainCamera.SetActive(false);
-            
-            SceneManager.LoadSceneAsync("(none) - (GameplayStuff) - (SingleLoad)", LoadSceneMode.Additive);
-            SceneManager.sceneLoaded += SceneLoaded;
+            StartCoroutine(LoadScenesAndTeleportPlayer());
         }
 
-        private void SceneLoaded(Scene arg0, LoadSceneMode loadSceneMode)
+        private IEnumerator LoadScenesAndTeleportPlayer()
         {
-            SceneManager.UnloadSceneAsync("(none) - (Main Menu) - (SingleLoad)");
-            SceneManager.LoadSceneAsync("(Krag1) - (AnonFlat) - (Passive)", LoadSceneMode.Additive);
-            SceneManager.LoadSceneAsync("(Krag1) - (AnonFlat) - (Interactive)", LoadSceneMode.Additive);
+            yield return SceneManager.LoadSceneAsync("(none) - (GameplayStuff) - (SingleLoad)", LoadSceneMode.Additive);
+            yield return SceneManager.LoadSceneAsync("(Krag1) - (AnonFlat) - (Interactive)", LoadSceneMode.Additive);
+            yield return SceneManager.LoadSceneAsync("(Krag1) - (AnonFlat) - (Passive)", LoadSceneMode.Additive);
 
-            SceneManager.sceneLoaded -= SceneLoaded;
+            SceneManager.UnloadSceneAsync("(none) - (Main Menu) - (SingleLoad)");
+
+            _playerManager = GameObject.FindWithTag("Player").GetComponent<PlayerManager>();
+            GameObject teleportDestination = GameObject.Find("TeleportDestination");
+            _playerManager.PlaceAt(teleportDestination.transform.position);
+
         }
 
         public void QuitGame()
         {
             Application.Quit();
         }
-
-    } 
+    }
 }
