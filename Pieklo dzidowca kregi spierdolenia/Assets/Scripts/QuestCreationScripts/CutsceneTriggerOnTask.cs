94781b88fb6d7d5391040cd613899b1958a46ef4
Salt Sugar <ltairxaltezi@gmail.com>
2024-07-07 16:58:16 +0200

    Pull request #345: GD-940 Ulepszyć interakcję z cyganem
    Merge in GD/gierka-jbzd from GD-940-ulepszyc-interakcje-z-cyganem to tech-demo
    
    Squashed commit of the following:
    
    commit b0c0a21db7a717edc8598840c40237d18be6ac42
    Author: Sugar <ltairxaltezi@gmail.com>
    Date:   Sun Jul 7 16:39:36 2024 +0200
    
        cygan sie nie teleportuje
    
    commit 9d1e9df64b4e4938f90d4725535419d03eaca8f2
    Author: Sugar <ltairxaltezi@gmail.com>
    Date:   Sun Jul 7 15:51:51 2024 +0200
    
        nie ten folder
    
    commit 0f10fdc79c8c1266144ce51670e14509fd2fd5bb
    Author: Sugar <ltairxaltezi@gmail.com>
    Date:   Sat Jul 6 22:58:34 2024 +0200
    
        dodanie cutsceny cygana

--- Pieklo dzidowca kregi spierdolenia/Assets/Scripts/QuestCreationScripts/CutsceneTriggerOnTask.cs
+++ Pieklo dzidowca kregi spierdolenia/Assets/Scripts/QuestCreationScripts/CutsceneTriggerOnTask.cs
@@ -5,38 +5,42 @@ using UnityEngine.Timeline;
 using UnityEngine.Playables;
 using Zenject;
 using MyBox;
+using jbzd.QuestSystem;
 
 namespace jbzd.QuestCreationScripts
 { 
     [RequireLayer("Player Triggers")]
     public class CutsceneTriggerOnTask : MonoBehaviour
     {
-        [field: SerializeField] private Quest _quest;
         [field: SerializeField] private TaskSO _task;
 
         public PlayableDirector playableDirector;
         private CutscenesManager _cutscenesManager;
+        private QuestManager _questManager;
 
         [Inject]
-        public void Construct(CutscenesManager cutscenesManager)
+        public void Construct(CutscenesManager cutscenesManager, QuestManager questManager)
         {
             _cutscenesManager = cutscenesManager;
+            _questManager = questManager;
         }
 
         private void OnTriggerEnter(Collider other)
         {
-
-            if (_quest is null || _task is null)
+            if (_task is null)
             {
-                Debug.Log($"There is a trigger without quest or task on {name}. You need to place a quest or delete component");
+                Debug.Log($"There is a trigger without task on {name}. You need to place a quest or delete component");
                 return;
             }
 
-            if (_quest.ActiveTask == _task)
+            foreach (var quest in _questManager.ActiveQuests)
             {
-                TimelineAsset timelineAsset = playableDirector.playableAsset as TimelineAsset;
-                _cutscenesManager.PlayCutscene(timelineAsset);
-            }
+                if (quest.ActiveTask == _task)
+                {
+                    TimelineAsset timelineAsset = playableDirector.playableAsset as TimelineAsset;
+                    _cutscenesManager.PlayCutscene(timelineAsset);
+                }
+            }               
         }
     }
 }
