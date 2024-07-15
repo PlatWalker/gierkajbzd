d6763dfc516f100120251164d86b9126015591a3
SilverWalker <silverwalkerjbzd@gmail.com>
2024-07-15 19:59:33 +0200

    Pull request #351: GD-952 przedwczesna aktywacja quest a
    Merge in GD/gierka-jbzd from GD-952-przedwczesna-aktywacja-quest-a to tech-demo
    
    Squashed commit of the following:
    
    commit 3f5424ac83e8e022f66b111352e445a5b3a0efd8
    Author: SilverWalker <silverwalkerjbzd@gmail.com>
    Date:   Sun Jul 14 12:42:10 2024 +0200
    
        little fix
    
    commit 7484e57cb71ab2e0f370a93d9b37204da6ddaf41
    Merge: 525a3b62 aea56228
    Author: SilverWalker <silverwalkerjbzd@gmail.com>
    Date:   Sun Jul 14 12:40:07 2024 +0200
    
        Merge branch 'tech-demo' into GD-952-przedwczesna-aktywacja-quest-a
    
    commit 525a3b6293c0bee74561a38dc279d2e0f5a8ed82
    Author: SilverWalker <silverwalkerjbzd@gmail.com>
    Date:   Sun Jul 14 12:39:12 2024 +0200
    
        fix previous mistake
    
    commit 9a4500a2639fbe6a1807bf0b508be110bc2c1da0
    Author: SilverWalker <silverwalkerjbzd@gmail.com>
    Date:   Sat Jul 13 13:58:37 2024 +0200
    
        little fix
    
    commit bacc2b77d14894d5ecff4cb9977936837f8b66b3
    Author: SilverWalker <silverwalkerjbzd@gmail.com>
    Date:   Sat Jul 13 13:46:40 2024 +0200
    
        now should be ok
    
    commit e324a933587e4f63637b6f89f4b64e5b652443e8
    Merge: bbc17354 7749e585
    Author: SilverWalker <silverwalkerjbzd@gmail.com>
    Date:   Wed Jul 10 18:57:50 2024 +0200
    
        Merge branch 'tech-demo' into GD-952-przedwczesna-aktywacja-quest-a
    
    commit bbc173547808415348e0b2336ac798c4a828f26b
    Author: Tomasz Strzep <46340313+wsumieniewiem123@users.noreply.github.com>
    Date:   Mon Jul 8 12:34:18 2024 +0200
    
        dodanie GoalTriggerOnInteraction
    
    commit 1520736396cc5c82328657a6809612b575b9157d
    Author: Tomasz Strzep <46340313+wsumieniewiem123@users.noreply.github.com>
    Date:   Tue Jul 2 22:13:09 2024 +0200
    
        poprawki
    
    commit f1f029b3f59d8c5af0cd763f38ca6a667f62411a
    Author: Tomasz Strzep <46340313+wsumieniewiem123@users.noreply.github.com>
    Date:   Sun Jun 30 16:05:21 2024 +0200
    
        poprawki
    
    commit c417db1a09850278a429ac58c2a81eaf1d1e23d7
    Author: Tomasz Strzep <46340313+wsumieniewiem123@users.noreply.github.com>
    Date:   Fri Jun 28 12:53:40 2024 +0200
    
        poprawki
    
    commit c8b69fe503ac28d85916ce4a7dbed08020fd1297
    Author: Tomasz Strzep <46340313+wsumieniewiem123@users.noreply.github.com>
    Date:   Wed Jun 26 22:26:37 2024 +0200
    
        poukładanie prefabów
    
    commit b64eb9bc1f04dbd2b9e1a7bc5c6547d2ee2ed104
    Author: Tomasz Strzep <46340313+wsumieniewiem123@users.noreply.github.com>
    Date:   Wed Jun 26 21:20:00 2024 +0200
    
        skrypty
    
    commit b67c5827170c519d28836b2add67b70cb0b7341d
    Author: Tomasz Strzep <46340313+wsumieniewiem123@users.noreply.github.com>
    Date:   Mon Jun 24 20:16:28 2024 +0200
    
        -
    
    commit 9089cb71f51fc8d0c5c6df2db553d668fb01e224
    Author: Tomasz Strzep <46340313+wsumieniewiem123@users.noreply.github.com>
    Date:   Mon Jun 24 19:41:36 2024 +0200
    
        poprawki
    
    commit 166baf2e7778dc46b6379db584ece754a3525205
    Author: Tomasz Strzep <46340313+wsumieniewiem123@users.noreply.github.com>
    Date:   Sun Jun 16 21:08:30 2024 +0200
    
        -

---
+++ Pieklo dzidowca kregi spierdolenia/Assets/Scripts/QuestCreationScripts/PlayGoalOnInteraction.cs
@@ -0,0 +1,30 @@
+using jbzd.Common.Interfaces;
+using jbzd.InteractSystem;
+using jbzd.QuestSystem;
+using jbzd.QuestSystem.QuestStructureElements;
+using UnityEngine;
+using Zenject;
+
+namespace jbzd.QuestCreationScripts
+{
+    public class PlayGoalOnInteraction : MonoBehaviour, IInteractable
+    {
+        private QuestManager _questManager;
+        
+        [SerializeField]
+        private GoalSO goalSo;
+        
+        [Inject]
+        public void Constructor(QuestManager questManager)
+        {
+            _questManager = questManager;
+        }
+
+        public void OnInteract()
+        {
+            Debug.Assert(goalSo is not null, $"There is no goalSO attached to component in {gameObject.name}");
+            
+            _questManager.MakeActorPlay(goalSo);
+        }
+    }
+}
