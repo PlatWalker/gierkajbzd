bdd6b28e26fa8b3b19235772dbccbd0eafbd76f4
Salt Sugar <ltairxaltezi@gmail.com>
2024-08-15 14:11:33 +0200

    Pull request #361: GD-997-nowe-escape-menu
    Merge in GD/gierka-jbzd from GD-997-nowe-escape-menu to tech-demo
    
    Squashed commit of the following:
    
    commit 6ee3cb066094a317d880c9bda6f3e8da4ef52c66
    Author: Sugar <ltairxaltezi@gmail.com>
    Date:   Wed Aug 14 18:36:00 2024 +0200
    
        nowe escape menu ui

--- Pieklo dzidowca kregi spierdolenia/Assets/Scripts/UI/EscapeMenu/EscapeMenuController.cs
+++ Pieklo dzidowca kregi spierdolenia/Assets/Scripts/UI/EscapeMenu/EscapeMenuController.cs
@@ -1,16 +1,34 @@
-﻿using System;
-using jbzd.Common;
-using jbzd.MainHero;
-using jbzd.Scenes.SceneLoader;
+﻿using jbzd.Common;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using Zenject;
+using jbzd.SavingSystem;
+using System.Collections.Generic;
 
 namespace jbzd.UI.EscapeMenu
 {
     public class EscapeMenuController : UserInterfaceController
     {
+        private SaveManager _saveManager;
+        [SerializeField]
+        private List<GameObject> _buttonParents = new();
+        [SerializeField]
+        private GameObject _buttonParent;
+
         public override bool InitialActivationState() => false;
-        
+
+        [Inject]
+        public void Constructor(SaveManager saveManager)
+        {
+            _saveManager = saveManager;
+        }
+
+        private void OnEnable()
+        {
+            _buttonParents.ForEach(p => { p.SetActive(false); });
+            _buttonParent.SetActive(true);
+        }
+
         public void QuitToMainMenu()
         {
             SceneManager.LoadScene(0, LoadSceneMode.Single);
@@ -20,6 +38,21 @@ namespace jbzd.UI.EscapeMenu
         {
             gameObject.SetActive(false);
             FreezeTime.Unfreeze();
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+
+        public void SaveGame()
+        {
+            _saveManager.SaveGame();
+        }
+
+        public void LoadGame()
+        {
+            _saveManager.LoadGame();
         }
     } 
 }
